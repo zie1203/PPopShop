fc13bb7001634efa64e94e14c81dc966473626ef
zie1203 <bermas.zictc@gmail.com>
2025-06-25 09:39:54 +0800

    Update Homebanner

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -5,7 +5,7 @@ const HomeBanner = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-5">
       <Title className="text-3xl md:text-4xl uppercase font-bold text-center">
-        Best Clothing Collection
+        Best Pinoy Pop Merchandise
       </Title>
       <p className="text-sm text-center text-lightColor/80 font-medium max-w-[480px]">
         Find everything you need to look and feel your best, and shop the latest
